dadb54bf2e61d8f8c197c0f5fe5d4ae77e68d505
GalloSimone <gallosimonecz@gmail.com>
2024-10-22 16:26:27 +0200

    end work + bonus

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
-const parola ="ciao"
-console.log(parola);
+const turnOn = (light, button) => {
+    light.setAttribute("src", "./img/yellow_lamp.png");
+    light.classList = "accesa";
+    button.innerText = "Spegni";
+    button.classList.replace("btn-warning", "btn-dark");
+  };
+
+  const turnOff = (light, button) => {
+    light.setAttribute("src", "./img/white_lamp.png");
+    light.classList = "spenta";
+    button.innerText = "accendi";
+    button.classList.replace("btn-dark", "btn-warning");
+  };
+  const buttonEl = document.querySelector("button");
+
+  const imgEl = document.querySelector("img");
+
+  buttonEl.addEventListener("click", () => {
+
+    let isOff = !imgEl.classList.contains("accesa");
+    
+    isOff ? turnOn(imgEl, buttonEl) : turnOff(imgEl, buttonEl);
+  });
+
+
+  
+
+  
